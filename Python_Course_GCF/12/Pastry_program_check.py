79f61650ec782b97494e0ee7de2434de1e49e2c8
Ju <94855929+PrincesLu@users.noreply.github.com>
2022-11-12 01:11:25 +0000

    Create Pastry_program_check.py

---
+++ Python_Course_GCF/12/Pastry_program_check.py
@@ -0,0 +1,11 @@
+biscuits = int(input('Score biscuits '))
+pastry = int(input('Sore pastry '))
+cake = int(input('Sore cake '))
+bread = int(input(' Scre bread '))
+freestyle = int(input('Score freestyle '))
+total = biscuits + pastry + cake + bread + freestyle
+
+if (biscuits >= 5) and (pastry >= 5) and (cake >= 5) and (bread >= 5) and (freestyle >= 5):
+    print('Congradulation you passed at the next stage')
+else:
+    print('Im\' sorry.
